807b51c84bc1bc6b2c96d1bb9f4d132985f78534
Juan Carlos <gurquiza8@gmail.com>
2024-09-18 14:39:08 -0600

    feature: lazyLoading for navigate bertween pages

---
+++ src/app/pages/about-page/about-page.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './about-page.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class AboutPageComponent {
+
+
+
+}
---
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './pricing-page.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class PricingPageComponent {
+
+
+
+}
