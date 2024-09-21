b567a7524323297a7b8b6fcae0e2557631eddce6
Juan Carlos <gurquiza8@gmail.com>
2024-09-21 16:05:21 -0600

    feature: working with queryparameters of router

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
 import { PokemonsListSkeletonComponent } from "./ui/pokemons-list-skeleton/pokemons-list-skeleton.component";
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { SimplePokemon } from '../../pokemons/interfaces';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'pokemons-page',
@@ -26,6 +29,16 @@ export default class PokemonsPageComponent implements OnInit {
 
   // public isLoading = signal(true);
 
+  private route = inject(ActivatedRoute);
+
+  public currentPage = toSignal<number>(
+    this.route.queryParamMap.pipe(
+      map( params => params.get('page') ?? '1' ),
+      map( page => ( isNaN(+page) ? 1 : +page ) ),
+      map( page => Math.max(1, page) )
+    )
+  );
+
   ngOnInit(): void {
 
     this.loadPokemons();
@@ -36,7 +49,9 @@ export default class PokemonsPageComponent implements OnInit {
   }
 
   public loadPokemons(page = 0) {
-    this.pokemonsService.loadPage(page).subscribe({
+    const pageToLoad = this.currentPage()! + page;
+
+    this.pokemonsService.loadPage(pageToLoad).subscribe({
       next: (resp) => {
         this.pokemons.set(resp);
       }
