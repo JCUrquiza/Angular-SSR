1e5a6a2a86353f10046860a28ad5e00a54fa9eac
Juan Carlos <gurquiza8@gmail.com>
2024-09-26 15:18:33 -0600

    feature:test. AppComponent first test only render elements html

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>
+  let app: AppComponent;
+  let compiled: HTMLDivElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [ provideRouter([]) ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    compiled = fixture.nativeElement;
   });
 
   it('should create the app', () => {
@@ -14,16 +24,13 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'pokemon-ssr' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokemon-ssr');
+  it(`should render the navbar and router-outlet`, () => {
+    // const navBarElement = fixture.nativeElement.querySelector('navbar');
+    // const routerOutletElement = fixture.nativeElement.querySelector('router-outlet');
+    // expect(navBarElement).not.toBeNull();
+    // expect(routerOutletElement).not.toBeNull();
+    expect(compiled.querySelector('navbar')).toBeTruthy();
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pokemon-ssr');
-  });
 });
