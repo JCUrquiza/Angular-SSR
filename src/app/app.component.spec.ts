e0231596744a9898cdfd6671de1c3d555e6d2009
Juan Carlos <gurquiza8@gmail.com>
2024-09-26 15:28:09 -0600

    feature:test. AppComponent. Creating mock for navbar component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 describe('AppComponent', () => {
 
@@ -8,11 +10,26 @@ describe('AppComponent', () => {
   let app: AppComponent;
   let compiled: HTMLDivElement;
 
+  @Component({
+    selector: 'navbar',
+    standalone: true,
+  })
+  class NavbarComponentMock {}
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
       providers: [ provideRouter([]) ]
-    }).compileComponents();
+    })
+    .overrideComponent(AppComponent, {
+      add: {
+        imports: [NavbarComponentMock]
+      },
+      remove: {
+        imports: [NavbarComponent]
+      }
+    })
+    .compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
     compiled = fixture.nativeElement;
@@ -31,6 +48,8 @@ describe('AppComponent', () => {
     // expect(routerOutletElement).not.toBeNull();
     expect(compiled.querySelector('navbar')).toBeTruthy();
     expect(compiled.querySelector('router-outlet')).toBeTruthy();
+
+    console.log(compiled);
   });
 
 });
