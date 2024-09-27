9ef2f5a7978fb2e25dd292a85b988ade86d1ed71
Juan Carlos <gurquiza8@gmail.com>
2024-09-26 19:07:00 -0600

    feature:test. PokemonsService test with httpServiceMock

---
+++ src/app/pokemons/services/pokemons.service.spec.ts
@@ -0,0 +1,72 @@
+import { TestBed } from '@angular/core/testing';
+import { PokemonsService } from './pokemons.service';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { PokeAPIResponse, SimplePokemon } from '../interfaces';
+
+const mockPokeApiResponse: PokeAPIResponse = {
+  count: 1302,
+  next: `https://pokeapi.co/api/v2/pokemon?offset=20&limit=20`,
+  previous: '',
+  results: [
+    {
+      name: 'bulbasaur',
+      url: `https://pokeapi.co/api/v2/pokemon/1/`,
+    },
+    {
+      name: 'ivysaur',
+      url: `https://pokeapi.co/api/v2/pokemon/2/`,
+    },
+  ],
+};
+
+const expectedPokemons: SimplePokemon[] = [
+  { id: '1', name: 'bulbasaur' },
+  { id: '2', name: 'ivysaur' },
+];
+
+const mockPokemon = {
+  id: 1,
+  name: 'bulbasaur'
+}
+
+describe(`PokemonsService`, () => {
+
+  let service: PokemonsService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
+    });
+    service = TestBed.inject(PokemonsService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  })
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should load a page of SimplePokemons', () => {
+    service.loadPage(1).subscribe((pokemons) => {
+      expect(pokemons).toEqual(expectedPokemons);
+    });
+
+    const req = httpMock.expectOne(
+      `https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`
+    );
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockPokeApiResponse);
+  });
+
+});
+
